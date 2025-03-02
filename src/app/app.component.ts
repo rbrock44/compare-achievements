c9dd94c0c0005c1f0c0ebabf49c7b50a82a2d1f2
rbrock44 <rbrock444@gmail.com>
2025-03-01 19:12:23 -0600

    add many more files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ComparisonComponent } from './components/comparison/comparison.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    ComparisonComponent,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
